55b1240114e828114c8fb6cee0ae706d9fbc4cf2
arif-fahrizal <fahrizalarif07@gmail.com>
2025-07-28 23:22:49 +0700

    FIX

--- Learning Vue/src/components/ClassAndStyleBinding.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-<template>
-  <RowComponent title="Text Interpolation">
-    <template v-slot:example1>
-      <p>{{ greeting }}</p>
-    </template>
-  </RowComponent>
-</template>
-<style scoped></style>
