4c30c3d43d87545407e4d75eb9e8dd3f3826b9a5
arif-fahrizal <fahrizalarif07@gmail.com>
2025-07-28 22:49:08 +0700

    Add Vue Forms project

---
+++ Learning Vue/src/components/OptionsComponent.vue
@@ -0,0 +1,27 @@
+<script>
+import RowComponent from './RowComponent.vue'
+
+export default {
+  components: {
+    RowComponent,
+  },
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+  },
+}
+</script>
+
+<template>
+  <RowComponent title="Options Component">
+    <template v-slot:example1>
+      <button @click="increment">Options components count is: {{ count }}</button>
+    </template>
+  </RowComponent>
+</template>
