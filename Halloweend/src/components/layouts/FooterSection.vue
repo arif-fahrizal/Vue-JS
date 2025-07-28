8a4c140ff3ffba54a660f958594bfdbd6d6e3e9f
arif-fahrizal <fahrizalarif07@gmail.com>
2025-07-28 22:48:40 +0700

    Add Todo App project

---
+++ Halloweend/src/components/layouts/FooterSection.vue
@@ -0,0 +1,67 @@
+<template>
+  <footer class="">
+    <div class="grid md:grid-cols-2 gap-7">
+      <div class="relative space-y-5">
+        <div class="flex gap-1">
+          <h1 class="text-2xl text-white font-bold">
+            Hallow<span class="text-secondary">ee</span>nd
+          </h1>
+          <img src="/src/assets/halloween.svg" alt="" />
+        </div>
+        <p class="text-xs text-white sm:text-base md:leading-loose md:tracking-wide lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum fringilla ultricies
+          eget amet blandit quis sit duis. Suspendisse fames venenatis volutpat urna dui. Quisque
+          non diam mi ullamcorper.
+        </p>
+        <img
+          class="absolute w-8 -bottom-6 right-8 -scale-x-100 md:-translate-x-10"
+          src="/src/assets/footer2-img.svg"
+          alt=""
+        />
+      </div>
+      <div class="flex flex-wrap justify-between items-center gap-7 text-white sm:justify-around">
+        <div class="">
+          <h3 class="w-auto mb-2 text-lg font-bold lg:text-xl">About</h3>
+          <div class="flex flex-col gap-3">
+            <a href="">About Us</a>
+            <a href="">Fetures</a>
+            <a href="">Career</a>
+          </div>
+        </div>
+        <div class="">
+          <h3 class="w-auto mb-2 text-lg font-bold lg:text-xl">Company</h3>
+          <div class="flex flex-col gap-3">
+            <a href="">Our Team</a>
+            <a href="">Partners</a>
+            <a href="">Questions</a>
+          </div>
+        </div>
+        <div class="">
+          <h3 class="w-auto mb-2 text-lg font-bold lg:text-xl">Services</h3>
+          <div class="flex flex-col gap-3">
+            <a href="">Support</a>
+            <a href="">Feedback</a>
+            <a href="">Contact</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid place-items-center w-full my-20">
+      <div class="flex items-center gap-5">
+        <a href="" class="grid place-items-center w-12 h-12 rounded-md bg-secondary">
+          <img src="/src/assets/social media/facebook.svg" alt="" />
+        </a>
+        <a href="" class="grid place-items-center w-12 h-12 rounded-md bg-secondary">
+          <img src="/src/assets/social media/instagram.svg" alt="" />
+        </a>
+        <a href="" class="grid place-items-center w-12 h-12 rounded-md bg-secondary">
+          <img src="/src/assets/social media/twitter.svg" alt="" />
+        </a>
+      </div>
+      <p class="mt-5 text-lg text-center font-medium text-white">
+        &copy; Copyright, Halloweend 2024, All rights reserved.
+      </p>
+    </div>
+  </footer>
+</template>
---
+++ Learning Vue/src/components/ReactiveButton.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import RowComponent from './RowComponent.vue'
+
+// Using ref
+const count = ref(0)
+
+// Using reactive
+const user = reactive({ name: 'John Doe', age: 30 })
+</script>
+<template>
+  <RowComponent title="Reactive Button">
+    <template #example1>
+      <span>Reactivity count is: </span><button @click="count++">{{ count }}</button>
+    </template>
+    <template #example2>
+      <div>
+        <p>Name: {{ user.name }}</p>
+        <span>Age: </span><button @click="user.age++">{{ user.age }}</button>
+      </div>
+    </template>
+  </RowComponent>
+</template>
+<style scoped></style>
