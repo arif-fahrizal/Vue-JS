a931889f91851d008123ca62a27fa67aef0ef763
arif-fahrizal <fahrizalarif07@gmail.com>
2025-07-28 22:47:25 +0700

    Add Modal Project

---
+++ Halloweend/src/components/layouts/CategorySection.vue
@@ -0,0 +1,38 @@
+<script setup>
+const categories = [
+  {
+    title: 'Ghost',
+    image: '/src/assets/category/category1-img.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.',
+  },
+  {
+    title: 'Witch',
+    image: '/src/assets/category/category2-img.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.',
+  },
+  {
+    title: 'Pumpkin',
+    image: '/src/assets/category/category3-img.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.',
+  },
+]
+</script>
+<template>
+  <section id="category" class="flex flex-col items-center">
+    <h2 class="mb-10 text-3xl font-bold text-white md:text-6xl">Category</h2>
+    <div class="grid place-items-center sm:grid-cols-3">
+      <div
+        class="w-5/6 grid place-items-center gap-2 text-center text-white"
+        v-for="({ title, image, description }, index) in categories"
+        :key="index"
+      >
+        <img :src="image" :alt="image" class="w-full" />
+        <h3 class="text-3xl font-bold">{{ title }}</h3>
+        <p class="w-full">{{ description }}</p>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ Learning Vue/src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import PrimaryButton from './components/PrimaryButton.vue'
+import PropsButton from './components/PropsButton.vue'
+import ReactiveButton from './components/ReactiveButton.vue'
+import ComputedProperties from './components/ComputedProperties.vue'
+import ConditionalRendering from './components/ConditionalRendering.vue'
+import ListRendering from './components/ListRendering.vue'
+import RowComponent from './components/RowComponent.vue'
+import OptionsComponent from './components/OptionsComponent.vue'
+import CompositionComponent from './components/CompositionComponent.vue'
+import EventHandling from './components/EventHandling.vue'
+import FallthroughAttribut from './components/FallthroughAttribut.vue'
+
+// Text Interpolation = output string
+const greeting = 'Hallo Guys'
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Example 1</th>
+        <th>Example 2</th>
+        <th>Example 3</th>
+        <th>Example 4</th>
+      </tr>
+    </thead>
+    <tbody>
+      <RowComponent title="Text Interpolation">
+        <template v-slot:example1>
+          <p>{{ greeting }}</p>
+        </template>
+      </RowComponent>
+      <PrimaryButton />
+      <OptionsComponent />
+      <CompositionComponent />
+      <PropsButton />
+      <ReactiveButton />
+      <ComputedProperties />
+      <ConditionalRendering />
+      <ListRendering />
+      <EventHandling />
+      <FallthroughAttribut class="fallthrough" @click="() => console.log('clicked')" />
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table {
+  max-width: 1200px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid black;
+}
+
+th {
+  width: 200px;
+  border: 1px solid #1e1e1e;
+}
+</style>
